267df40963e83f3a5918dda5bdb0669d6325c197
unknown <you@example.com>
2020-06-20 04:48:32 -0400

    added json indent

--- api/views.py
+++ api/views.py
@@ -10,13 +10,13 @@ def testing_data_raw_list(request):
     if request.method == 'GET':
         data = testing_data_raw.objects.all()
         serializer = testing_data_raw_serializer(data, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return JsonResponse(serializer.data, safe=False,  json_dumps_params={'indent': 2})
     
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = testing_data_raw_serializer(data=data)
 
         if serializer.is_valid():
-            return JsonResponse(serializer.data, status=201)
+            return JsonResponse(serializer.data, status=201,  json_dumps_params={'indent': 2})
         
-        return JsonResponse(serializer.errors, status=400)
+        return JsonResponse(serializer.errors, status=400,  json_dumps_params={'indent': 2})
