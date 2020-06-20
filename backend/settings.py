95005e0342002b5eb3a433dd77a588c6a6e9e708
unknown <you@example.com>
2020-06-19 23:31:44 -0400

    please work

--- backend/settings.py
+++ backend/settings.py
@@ -34,15 +34,13 @@ SITE_ID = 1
 # Application definition
 
 INSTALLED_APPS = [
+    "django.contrib.sites",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django.contrib.sites",
-    'allauth',                                                                 
-    'allauth.account',
     "hello",
     "rest_framework",
     "api",
